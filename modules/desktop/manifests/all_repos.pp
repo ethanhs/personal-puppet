d30e326090baad5d81f6396cfe9daf990ff090b4
Ethan Smith <ethan@ethanhs.me>
2020-04-03 15:50:29 -0700

    Add hiera eyaml setup

---
+++ modules/desktop/manifests/all_repos.pp
@@ -0,0 +1,14 @@
+class desktop::all_repos {
+  $all_repos_config_path = "C:\\Users\\ethanhs\\.all_repos\\all_repos.json"
+  windows_env { 'all repos config path':
+    ensure    => present,
+    variable => "ALL_REPOS_CONFIG_FILENAME",
+    value     => $all_repos_config_path
+  }
+
+  file { $all_repos_config_path:
+    ensure => file,
+    content => lookup('desktop::all_repos::config_file_content')
+  }
+
+}
