fe94be3a86382350c42f5a4b29cbf1df73a5b68b
Ethan Smith <ethan@ethanhs.me>
2020-04-03 16:29:36 -0700

    Lint fixes

--- modules/desktop/manifests/all_repos.pp
+++ modules/desktop/manifests/all_repos.pp
@@ -2,12 +2,12 @@ class desktop::all_repos {
   $all_repos_config_path = "C:\\Users\\ethanhs\\.all_repos\\all_repos.json"
   windows_env { 'all repos config path':
     ensure    => present,
-    variable => "ALL_REPOS_CONFIG_FILENAME",
+    variable  => 'ALL_REPOS_CONFIG_FILENAME',
     value     => $all_repos_config_path
   }
 
   file { $all_repos_config_path:
-    ensure => file,
+    ensure  => file,
     content => lookup('desktop::all_repos::config_file_content')
   }
 
--- modules/desktop/manifests/gpg.pp
+++ modules/desktop/manifests/gpg.pp
@@ -1,17 +1,17 @@
 class desktop::gpg {
 
   file { 'gpg-agent.conf':
-    path => "${::windows_env['APPDATA']}\\gnupg\\gpg-agent.conf",
-    source => "puppet:///modules/desktop/gpg-agent.conf"
+    path   => "${::windows_env['APPDATA']}\\gnupg\\gpg-agent.conf",
+    source => 'puppet:///modules/desktop/gpg-agent.conf'
   }
 
   file { 'gpg.conf':
-    path => "${::windows_env['APPDATA']}\\gnupg\\gpg.conf",
-    source => "puppet:///modules/desktop/gpg.conf"
+    path   => "${::windows_env['APPDATA']}\\gnupg\\gpg.conf",
+    source => 'puppet:///modules/desktop/gpg.conf'
   }
 
   exec { 'restart gpg agent':
-    command  => "gpgconf.exe --reload gpg-agent; gpgconf.exe --list-options gpg-agent",
+    command  => 'gpgconf.exe --reload gpg-agent; gpgconf.exe --list-options gpg-agent',
     provider => powershell,
   }
 
