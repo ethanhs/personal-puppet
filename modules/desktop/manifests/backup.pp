1f7428a6e5a751637ddc017544ce240204d34a64
Ethan Smith <ethan@ethanhs.me>
2020-04-25 21:22:06 -0700

    More lint fixes

--- modules/desktop/manifests/backup.pp
+++ modules/desktop/manifests/backup.pp
@@ -1,5 +1,5 @@
 class desktop::backup {
-  file { "${windows_env['USERPROFILE']}\\backup.bat":
+  file { "${::windows_env['USERPROFILE']}\\backup.bat":
     ensure  => file,
     content => lookup('desktop::backup::script_content')
   }
