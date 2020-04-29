a87e18b16c889abeaa4336c145c21f8e4615962e
Ethan Smith <ethan@ethanhs.me>
2020-04-28 21:25:45 -0700

    Restructure laptop packages, install code deb

--- modules/laptop/manifests/packages.pp
+++ modules/laptop/manifests/packages.pp
@@ -1,77 +1,22 @@
 class laptop::packages {
-    # General pacakges
-    $packages = [
-      # utilities
-      'curl',
-      'dos2unix',
-      'graphviz',
 
-      # Python
-      'python3-venv',
-      'python3-dev',
-      'python3-dbg',
+  apt::key { 'vscode':
+    id     => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
+    server => 'keyserver.ubuntu.com',
+  }
 
-      # editors
-      'vim',
+  apt::source { 'vscode':
+    architecture => 'amd64',
+    location     => 'http://packages.microsoft.com/repos/vscode',
+    release      => 'stable',
+    repos        => 'main',
+    require      => Apt::Key['vscode'],
+  }
 
-      # media
-      'vlc',
-
-      # deps
-      'software-properties-common',
-    ]
-
-    # TODO: more packages? https://github.com/ocf/puppet/blob/master/modules/ocf_desktop/manifests/packages.pp
-
-    package { $packages:  ensure   => latest,  }
-
-    # deps for various things
-    $deps = [
-      # git
-      'libcurl4-openssl-dev', 'libssl-dev', 'zlib1g-dev',
-      # cpython
-      'libffi-dev',
-      'libreadline-dev',
-      'libsqlite3-dev',
-      'ruby-dev',
-    ]
-    package { $deps: ensure => 'latest' }
-
-    # debian packaging deps
-    package {'aptitude': ensure => 'latest' }
-    package {'devscripts': ensure => 'latest' }
-    package {'git-buildpackage': ensure => 'latest' }
-
-    $deadsnakes_pkgs = ['python3.7-dev', 'python3.8-dev', 'python3.9-dev']
-    apt::ppa { 'ppa:deadsnakes/ppa': } ->
-    package { $deadsnakes_pkgs:
-      ensure  => 'latest',
-      require => Exec['apt_update'],
-    }
-
-    # TODO: graphics drivers
-    # https://github.com/ocf/puppet/blob/master/modules/ocf_desktop/manifests/drivers.pp
-
-    # TODO: use OCF apt? or another one? https://github.com/ocf/puppet/blob/4e37cbedd228d89bc2f32234dbb4fc54114faa9d/modules/ocf/manifests/apt.pp
-    # TODO: LLVM apt
-
-    apt::key { 'vscode':
-      id     => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
-      server => 'keyserver.ubuntu.com',
-    }
-
-    apt::source { 'vscode':
-      architecture => 'amd64',
-      location     => 'http://packages.microsoft.com/repos/vscode',
-      release      => 'stable',
-      repos        => 'main',
-      require      => Apt::Key['vscode'],
-    }
-
-    apt::key { 'google':
-      id     => 'EB4C1BFD4F042F6DDDCCEC917721F63BD38B4796',
-      source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub';
-    }
+  apt::key { 'google':
+    id     => 'EB4C1BFD4F042F6DDDCCEC917721F63BD38B4796',
+    source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub';
+  }
 
   # Chrome creates /etc/apt/sources.list.d/google-chrome.list upon
   # installation, so we use the name 'google-chrome' to avoid duplicates
@@ -86,8 +31,64 @@ class laptop::packages {
       require  => Apt::Key['google'];
   }
 
+  # General pacakges
+  $packages = [
+    # utilities
+    'curl',
+    'dos2unix',
+    'graphviz',
 
-  $snaps = ['spotify', 'discord', 'code', 'skype']
+    # Python
+    'python3-venv',
+    'python3-dev',
+    'python3-dbg',
+
+    # editors
+    'code',
+    'vim',
+
+    # media
+    'vlc',
+
+    # deps
+    'software-properties-common',
+  ]
+
+  # TODO: more packages? https://github.com/ocf/puppet/blob/master/modules/ocf_desktop/manifests/packages.pp
+
+  package { $packages:  ensure   => latest,  }
+
+  # deps for various things
+  $deps = [
+    # git
+    'libcurl4-openssl-dev', 'libssl-dev', 'zlib1g-dev',
+    # cpython
+    'libffi-dev',
+    'libreadline-dev',
+    'libsqlite3-dev',
+    'ruby-dev',
+  ]
+  package { $deps: ensure => 'latest' }
+
+  # debian packaging deps
+  package {'aptitude': ensure => 'latest' }
+  package {'devscripts': ensure => 'latest' }
+  package {'git-buildpackage': ensure => 'latest' }
+
+  $deadsnakes_pkgs = ['python3.7-dev', 'python3.8-dev', 'python3.9-dev']
+  apt::ppa { 'ppa:deadsnakes/ppa': } ->
+  package { $deadsnakes_pkgs:
+    ensure  => 'latest',
+    require => Exec['apt_update'],
+  }
+
+  # TODO: graphics drivers
+  # https://github.com/ocf/puppet/blob/master/modules/ocf_desktop/manifests/drivers.pp
+
+  # TODO: use OCF apt? or another one? https://github.com/ocf/puppet/blob/4e37cbedd228d89bc2f32234dbb4fc54114faa9d/modules/ocf/manifests/apt.pp
+  # TODO: LLVM apt
+
+  $snaps = ['spotify', 'discord', 'skype']
   package { $snaps:
     ensure          => latest,
     provider        => snap,
