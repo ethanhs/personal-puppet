ea57c09722bc1e5eb9bdacbf8e488180cbfc8cc3
Ethan Smith <ethan@ethanhs.me>
2020-05-27 22:35:55 -0700

    Clobber env for all_repos

--- modules/desktop/manifests/all_repos.pp
+++ modules/desktop/manifests/all_repos.pp
@@ -1,9 +1,10 @@
 class desktop::all_repos {
   $all_repos_config_path = "${::windows_env['USERPROFILE']}\\.all_repos\\all_repos.json"
   windows_env { 'all repos config path':
-    ensure   => present,
-    variable => 'ALL_REPOS_CONFIG_FILENAME',
-    value    => $all_repos_config_path
+    ensure    => present,
+    mergemode => clobber,
+    variable  => 'ALL_REPOS_CONFIG_FILENAME',
+    value     => $all_repos_config_path
   }
 
   file { $all_repos_config_path:
