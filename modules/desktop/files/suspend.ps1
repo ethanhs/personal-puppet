b495b5b0f2b936a8fe1c9754fc8fe2ebd4dfd597
Ethan Smith <ethan@ethanhs.me>
2020-10-21 15:28:27 -0700

    Rewrite suspend script

--- modules/desktop/files/suspend.ps1
+++ modules/desktop/files/suspend.ps1
@@ -1,3 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 $PowerState = [System.Windows.Forms.PowerState]::Suspend
-[System.Windows.Forms.Application]::SetSuspendState($PowerState, $false, $false)
+$wshell = New-Object -ComObject Wscript.Shell
+$answer = $wshell.Popup("Do you want to suspend?",10,"Question",32+4)
+if ($answer -ne 7) {
+    [System.Windows.Forms.Application]::SetSuspendState($PowerState, $false, $false)
+}
